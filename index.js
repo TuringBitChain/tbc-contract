7014a51c47ffcdef7b1af4f7a790d9e2958f4e57
Joe <joe@turingbitchain.com>
2024-12-26 21:05:24 +0800

    add multisig

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-'use strict'
+var contract = module.exports;
 
-var contract = module.exports
-
-// module information
-contract.version = 'v' + require('./package.json').version
+contract.version = 'v' + require('./package.json').version;
 contract.versionGuard = function (version) {
   if (version !== undefined) {
     var message = `
       More than one instance of tbc found.
       Please make sure to require tbc and check that submodules do
-      not also include their own tbc dependency.`
-    console.warn(message)
+      not also include their own tbc dependency.`;
+    console.warn(message);
   }
-}
-contract.versionGuard(globalThis.contract)
-globalThis.contract = contract.version
-
-contract.FT = require('./lib/contract/ft')
-contract.poolNFT = require('./lib/contract/poolNFT')
-contract.API = require('./lib/api/api')
-contract.NFT = require('./lib/contract/nft')
+};
+contract.versionGuard(globalThis.contract);
+globalThis.contract = contract;
+contract.FT = require("./lib/contract/ft.js");
+contract.poolNFT = require("./lib/contract/poolNFT.js");
+contract.API = require("./lib/api/api.js");
+contract.NFT = require("./lib/contract/nft.js");
